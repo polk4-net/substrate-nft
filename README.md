f1391991c6cd2e608763322150964c3e5960a3fe
Nick <mshulhin@gmail.com>
2021-06-29 17:55:07 +1000

    Genesis

---
+++ README.md
@@ -0,0 +1,33 @@
+##Substrate !ink NFT simple implementation
+****
+This is a simple working version of base NFT smart contract written using latest (as of this date) !ink `3.0.0-rc3`.
+
+To run all tests:
+``` 
+cargo +nightly test
+```
+To compile to WASM:
+``` 
+cargo +nightly contract build 
+```
+Original wasm size: 47.4K, Optimized: 26.3K
+
+Once your contract is compiled you can find artifacts in:
+<PROJECT_DIR>/target/ink
+
+- nft.contract (code + metadata)
+- nft.wasm (the contract's code)
+- metadata.json (the contract's metadata)
+
+If you are facing any issues please follow the setup step of substrate environment at:
+
+https://substrate.dev/substrate-contracts-workshop/#/0/setup
+
+###Testing on using Polk4 canvas network
+****
+You can upload this contract and interract with it either via your own local Canvas node or using Polk4 Canvas testnet.
+
+- Head to https://paritytech.github.io/canvas-ui/
+- In node settings input custom node endpoint: wss://canvas.polk4.net
+- Upload `nft.contract` into the node via UI  
+- Instantiate a contract and make RPC calls/transactions 🎉
---
+++ lib.rs
@@ -0,0 +1,109 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+use ink_lang as ink;
+
+#[ink::contract]
+mod nft {
+    #[ink(storage)]
+    pub struct NFT {
+        owner: AccountId,
+        id_to_owner: ink_storage::collections::HashMap<u64, AccountId>,
+        owner_to_token_count: ink_storage::collections::HashMap<AccountId, u64>,
+    }
+
+    impl NFT {
+        #[ink(constructor)]
+        pub fn default() -> Self {
+            Self {
+                owner: Self::env().caller(),
+                id_to_owner: ink_storage::collections::HashMap::new(),
+                owner_to_token_count: ink_storage::collections::HashMap::new(),
+            }
+        }
+
+        #[ink(message)]
+        pub fn mint(&mut self, receiver: AccountId, token_id: u64) -> bool {
+            self.id_to_owner.insert(token_id, receiver);
+            let existing_number = self.owner_to_token_count.get(&receiver);
+            if let Some(n) = existing_number {
+                self.owner_to_token_count.insert(receiver, n + 1);
+            } else {
+                self.owner_to_token_count.insert(receiver, 1);
+            }
+            true
+        }
+
+        fn is_token_owner(&self, account: &AccountId, token_id: u64) -> bool {
+            let owner = self.id_to_owner.get(&token_id);
+            match owner {
+                Some(acc) => return if acc != account {
+                    false
+                } else {
+                    true
+                },
+                None => false,
+            }
+        }
+
+        #[ink(message)]
+        pub fn transfer(&mut self, from: AccountId, to: AccountId, token_id: u64) -> bool {
+            if !self.is_token_owner(&from, token_id) {
+                return false;
+            }
+
+            self.id_to_owner.insert(token_id, to);
+
+            let from_owner_count = *self.owner_to_token_count.get(&from).unwrap_or(&0);
+            let to_owner_count = *self.owner_to_token_count.get(&to).unwrap_or(&0);
+
+            self.owner_to_token_count.insert(from, from_owner_count - 1);
+            self.owner_to_token_count.insert(to, to_owner_count + 1);
+            true
+        }
+
+        #[ink(message)]
+        pub fn get_token_count_for_account(&self, account: AccountId) -> u64 {
+            let number = self.owner_to_token_count.get(&account);
+            if let None = number {
+                return 0;
+            }
+            return *number.unwrap();
+        }
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+
+        use ink_lang as ink;
+
+        #[ink::test]
+        fn default_state() {
+            let nft = NFT::default();
+            assert_eq!(nft.get_token_count_for_account(AccountId::from([0x1; 32])), 0);
+        }
+
+        #[ink::test]
+        fn mint() {
+            let mut nft = NFT::default();
+            let account_one = AccountId::from([0x1; 32]);
+            let token_id = 95;
+
+            nft.mint(account_one, token_id);
+            assert_eq!(nft.get_token_count_for_account(account_one), 1);
+        }
+
+        #[ink::test]
+        fn transfer() {
+            let mut nft = NFT::default();
+            let account_one = AccountId::from([0x1; 32]);
+            let account_two = AccountId::from([0x2; 32]);
+            let token_id = 95;
+
+            nft.mint(account_one, token_id);
+            nft.transfer(account_one, account_two, token_id);
+            assert_eq!(nft.get_token_count_for_account(account_one), 0);
+            assert_eq!(nft.get_token_count_for_account(account_two), 1);
+        }
+    }
+}
